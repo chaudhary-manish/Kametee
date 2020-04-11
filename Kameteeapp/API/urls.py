30d29112f3f334cf38b47b17cc7a32b1285f677d
Manish Chaudhary <chaudhary94rc@gmail.com>
2020-04-12 01:54:18 +0530

    add test url

---
+++ Kameteeapp/API/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from Kameteeapp.API.views import *
+
+
+urlpatterns = [
+    path('/',RegisterUser,name='RegisterUser')
+    
+]
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Kametee
+
+ sudo apt-get 
+ 1: update/upgrade
+ 2: install python3-venv
