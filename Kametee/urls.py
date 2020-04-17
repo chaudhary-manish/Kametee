64f96fedb0d164fd70f3da3737f058e078e46dbd
Manish Chaudhary <chaudhary94rc@gmail.com>
2020-04-18 01:12:04 +0530

    change url settings

--- Kametee/urls.py
+++ Kametee/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('kametee',include('Kameteeapp.API.urls')),
+    path('kametee/',include('Kameteeapp.API.urls')),
 ]
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
  sudo apt-get 
  1: update/upgrade
  2: install python3-venv
+ 3: Inatall gunicorn
+ 4: Inatsall Nginx server
+ 5: Set up supervisors
+ 6: Set Database
+ 7: set Auth token Confi
+ 8:  
