01627264635a619d51a196650233d4dd4e35751a
Manish Chaudhary <chaudhary94rc@gmail.com>
2020-07-14 12:35:53 +0530

    change get method

--- Kameteeapp/API/views.py
+++ Kameteeapp/API/views.py
@@ -269,11 +269,13 @@ def groupmember_update(request,id):
             status = UserGroup.objects.get(id__in=GroupMember.objects.filter(id=id).values('UserGroup_id')).groupStatus
             UsergroupID = GroupMember.objects.filter(id=id).values('UserGroup_id')[0]  
             checkuserexist  = GroupMember.objects.filter(UserGroup_id=UsergroupID['UserGroup_id'],Mobilenumber = Mobilenumber).count()
+
+            checkuserexistname  = GroupMember.objects.filter(UserGroup_id=UsergroupID['UserGroup_id'],UserName=UserName).count()
            
             if status == 5:
                 if request.method == 'PUT':
-                    if checkuserexist > 0:
-                         GroupMemberupdate =  GroupMember.objects.filter(id=id).update(UserName=UserName)
+                    if checkuserexist > 0 or  checkuserexistname > 0 :
+                          return Response({'Message' :"Name or Mobile Number already exist for this group ",'Response' :True},status=200)
                     else:
                         GroupMemberupdate =  GroupMember.objects.filter(id=id).update(Mobilenumber=Mobilenumber,UserName=UserName)
                     return Response({'Message' :"User update successfully ",'Response' :True},status=200)
@@ -882,41 +884,93 @@ def Get_Terms_Condition(request):
         userid = Token.objects.get(key=token).user_id
         if userid is not None:      
             data={
-            'TermsCondition' : """This Privacy Policy explains and governs how Kametee Pvt. Ltd. (hereinafter "Company", "we", "our" or  "us") treats the Personal Information collected, used and disclosed by you on any of your use of our Website or Application (collectively, the "Services") or when you otherwise interact with us, including your Personal Information. "Personal Information" is defined as any information that relates to a natural person, that would be you if you are an individual), which, either directly or indirectly, in combination with other information, is capable of identifying such person. We may change this Privacy Policy from time to time. If we make changes, we will notify you by revising the date at the top of the policy and, in some cases, we may provide you with more prominent notice (such as adding a statement to our homepage or sending you an email notification).
-                 We encourage you to review the Privacy Policy whenever you access the Services to stay informed about our information practices and the ways you can help protect your privacy.
-                 All the capitalized terms used in the Privacy Policy, if not otherwise defined, will have the same meaning as in our Terms of Use.
+                
+            'privacypolicy' : """ \n This Privacy Policy explains and governs how Kametee Pvt. Ltd. (hereinafter "Company", "we", "our" or  "us") treats the Personal Information collected, used and disclosed by you on any of your use of our Website or Application (collectively, the "Services") or when you otherwise interact with us, including your Personal Information. "Personal Information" is defined as any information that relates to a natural person, that would be you if you are an individual), which, either directly or indirectly, in combination with other information, is capable of identifying such person. We may change this Privacy Policy from time to time. If we make changes, we will notify you by revising the date at the top of the policy and, in some cases, we may provide you with more prominent notice (such as adding a statement to our homepage or sending you an email notification).
+                 We encourage you to review the Privacy Policy whenever you access the Services to stay informed about our information practices and the ways you can help protect your privacy. \n
+                 All the capitalized terms used in the Privacy Policy, if not otherwise defined, will have the same meaning as in our Terms of Use. \n
                     1: Nature of Personal Information Collected
                        We collect some Personal Information when you register with us, such as your name, address, phone number, email address, birth date, gender, pin code, occupation, industry and personal preferences and interests relevant to us. Once you register with us and sign in to our Services, we automatically receive and record information on our server logs from your browser, including your IP address, cookie information, and the page you request.
-                       All the capitalized terms used in the Privacy Policy, if not otherwise defined, will have the same meaning as in our Terms of Use.
+                       All the capitalized terms used in the Privacy Policy, if not otherwise defined, will have the same meaning as in our Terms of Use.\n
 
                     2: Use of Personal Information
-                       We shall not sell your Personal Information. The information you provide to us or we collect is used for the customization of Services or content to your requirement, providing the Services, communicate with you, conduct research and improve our Services, [and provide anonymous, aggregated reporting for internal and external clients].
+                       We shall not sell your Personal Information. The information you provide to us or we collect is used for the customization of Services or content to your requirement, providing the Services, communicate with you, conduct research and improve our Services, [and provide anonymous, aggregated reporting for internal and external clients]. \n
 
                     3: Confidentiality and Security
-                       We provide the Services through secure servers and all data collected in course of your use of the Services is stored and controlled on servers located in Hyderabad, India and USA. We have stringent security measures in place to protect the loss, misuse, and alteration of the information under our control. Once your information is in our possession we adhere to strict security guidelines, protecting it against unauthorized access.
-                       We limit access to Personal Information about you to employees, affiliates and/or persons who we believe reasonably need that information to provide services to you or in order to operate and improve the Services.
+                       We provide the Services through secure servers and all data collected in course of your use of the Services is stored and controlled on servers located in Hyderabad, India and USA. We have stringent security measures in place to protect the loss, misuse, and alteration of the information under our control. Once your information is in our possession we adhere to strict security guidelines, protecting it against unauthorized access.\n
+                       We limit access to Personal Information about you to employees, affiliates and/or persons who we believe reasonably need that information to provide services to you or in order to operate and improve the Services. \n
 
                     4: Accessing, Changing and Protecting Your Information
-                       You can access and edit your account information at any time. While you are our Member, we will send you certain communications relating to the service, such as service announcements, administrative messages and a newsletter. These communications are considered as part of your account, and you can opt out of receiving them by updating your account settings or only opt-out of receiving them by deleting your account with us. You can delete your account by contacting us or visiting your account page.
+                       You can access and edit your account information at any time. While you are our Member, we will send you certain communications relating to the service, such as service announcements, administrative messages and a newsletter. These communications are considered as part of your account, and you can opt out of receiving them by updating your account settings or only opt-out of receiving them by deleting your account with us. You can delete your account by contacting us or visiting your account page.\n
 
                     5: Disclosure of Your Personal Information
-                       We may be required to disclose information collected by us, including your Personal Information, to comply with our legal obligations or requests from law enforcement agencies, to resolve any disputes that we may have with any of our users, and enforce our agreements with third parties, or to carry out any other purpose for which the information was collected. We may also share aggregated or de-identified information, which cannot reasonably be used to identify you. The Service may offer social sharing features and other integrated tools (such as the Facebook "Like" button), which let you share actions you take on the Service with other media, and vice versa. The use of such features enables the sharing of information with your friends or the public, depending on the settings you establish with the entity that provides the social sharing feature. For more information about the purpose and scope of data collection and processing in connection with social sharing features, please visit the privacy policies of the entities that provide those features. We may also share our users' information with any successor in interest if the Company is acquired by or merged with another company, or sells a significant portion of its assets, or all of its assets, to any third party.
+                       We may be required to disclose information collected by us, including your Personal Information, to comply with our legal obligations or requests from law enforcement agencies, to resolve any disputes that we may have with any of our users, and enforce our agreements with third parties, or to carry out any other purpose for which the information was collected. We may also share aggregated or de-identified information, which cannot reasonably be used to identify you. The Service may offer social sharing features and other integrated tools (such as the Facebook "Like" button), which let you share actions you take on the Service with other media, and vice versa. The use of such features enables the sharing of information with your friends or the public, depending on the settings you establish with the entity that provides the social sharing feature. For more information about the purpose and scope of data collection and processing in connection with social sharing features, please visit the privacy policies of the entities that provide those features. We may also share our users' information with any successor in interest if the Company is acquired by or merged with another company, or sells a significant portion of its assets, or all of its assets, to any third party.\n
 
                     6: Consent
                        By using the Services by providing your information, you consent to the collection and use of the information you disclose on the Website in accordance with this Privacy Policy, including but not limited to your consent for sharing your information as per this privacy policy. If we decide to change our
-                       privacy policy, we will post those changes on this page so that you are always aware of what information we collect, how we use it, and under what circumstances we disclose it.
+                       privacy policy, we will post those changes on this page so that you are always aware of what information we collect, how we use it, and under what circumstances we disclose it.\n
 
                     7: Grievance Officer
-                        In accordance with Information Technology Act 2000 and rules made there under, the name and contact details of the Grievance Officer are provided below:
+                        In accordance with Information Technology Act 2000 and rules made there under, the name and contact details of the Grievance Officer are provided below: \n
 
-                       Manish Chaudhary
+                       
+                    Manish Chaudhary \n
 
-                    Kametee Pvt. Ltd.
-                    Phone: +91 8279463818
-                    Email: chaudhary94rc@gmail.com
-                    C-603, Amarapali Empire, Crossing Republic, Ghaziabad            
+                    Kametee Pvt. Ltd.\n
+                    Phone: +91 8279463818\n
+                    Email: chaudhary94rc@gmail.com \n
+                    C-603, Amarapali Empire, \n
+                    Crossing Republic, Ghaziabad \n            
             """,
-            'privacypolicy' : 'Hi Manish'
+            'TermsCondition' : """ \n
+                            IMPORTANT - READ THESE TERMS CAREFULLY BEFORE DOWNLOADING, INSTALLING OR USING THIS SOFTWARE. BY DOWNLOADING OR
+                            INSTALLING THIS SOFTWARE, YOU ACKNOWLEDGE THAT YOU HAVE READ THIS LICENSE AGREEMENT, THAT YOU UNDERSTAND IT, AND 
+                            THAT YOU AGREE TO BE BOUND BY ITS TERMS. IF YOU ARE ENTERING INTO THIS AGREEMENT ON BEHALF OF A COMPANY OR OTHER 
+                            LEGAL ENTITY, YOU REPRESENT THAT YOU HAVE THE AUTHORITY TO BIND SUCH ENTITY AND ITS AFFILIATES TO THESE TERMS AND 
+                            CONDITIONS, IN WHICH CASE THE TERMS "YOU" OR "YOUR" SHALL REFER TO ITS AFFILIATES. IF YOU DO NOT HAVE SUCH AUTHORITY,
+                            OR IF YOU DO NOT AGREE WITH THESE TERMS AND CONDITIONS, YOU MUST NOT DOWNLOAD OR INSTALL THIS SOFTWARE. \n
+                            \n
+                            . Grant of Software License for Free Trial Period \n
+                                Subject to the terms and conditions and except as otherwise provided in this License Agreement, Kamitee grants to you
+                                a limited, non­ exclusive, non-transferable and non-assignable license to evaluate Kamitee software, modules, and
+                                feature(s) (the "Software") for Your personal purposes only. As used herein the "Software" is subject to licenses, 
+                                you do not have any rights in or to the Software except as expressly granted in this License Agreement. Kametee 
+                                retains all copyright, trademarks, patent, and other intellectual property rights to the Software. You acknowledge 
+                                that the Software, all copies of the Software, any derivative works, compilations, and collective works of the
+                                Software, and any know-how and trade secrets related to the Software are the sole and exclusive property of 
+                                Kametee Private Limited and contain Kamitee's confidential and proprietary materials \n
+                            . General Limitations \n
+                            Except as otherwise expressly provided under this License Agreement, you shall have no right, and you specifically agree not to: \n
+
+                               a: Utilize the Software beyond the applicable Term; \n
+                               b: Transfer, assign or sublicense Your license rights to any other person, and any such attempted transfer, assignment or sublicense shall be void; \n
+                               c: Provide, divulge, disclose, or make available to, or permit the use of the Software by any third party; \n
+                               d: Sell, resell, license, sublicense, distribute, rent or lease the Software or include the Software in a service bureau or outsourcing offering; \n
+                               e: Make error corrections to or otherwise modify or adapt the Software or create derivative works based upon the Software, or to permit third parties
+                                  to do the same; \n
+                               f: Decompile, decrypt, reverse engineer, disassemble or otherwise reduce the Software to human-readable form, or to permit third parties to do the same; \n
+                               g: Circumvent or disable any features or technological protection measures in the Software; \n
+
+                            . Limited Warranty & Limitation of Liability \n
+                                THE SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND. ALL EXPRESS, IMPLIED OR STATUTORY CONDITIONS,REPRESENTATIONS, AND WARRANTIES INCLUDING,
+                                WITHOUT LIMITATION, ANY IMPLIED WARRANTY OR CONDITION OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON­ INFRINGEMENT, SATISFACTORY QUALITY
+                                OR ARISING FROM A COURSE OF DEALING, LAW, USAGE, OR TRADE PRACTICE, ARE HEREBY EXCLUDED TO THE MAXIMUM EXTENT ALLOWED BY APPLICABLE LAW. NEITHER 
+                                KAMITEE NOR ITS LICENSORS SHALL BE LIABLE FOR YOUR ACTION, OR FAILURE TO ACT, IN RELIANCE ON ANY INFORMATION FURNISHED AS PART OF THE SOFTWARE. 
+                                YOU ARE SOLELY RESPONSIBLE FOR MAINTAINING THE SECURITY OF YOUR NETWORK AND COMPUTER SYSTEMS. NEITHER KAMITEE NOR ITS LICENSORS REPRESENT, WARRANT, OR GUARANTEE THAT
+                                (A) SECURITY THREATS, MALICIOUS CODE AND/OR VULNERABILITIES WILL BE IDENTIFIED, OR (B) THE CONTENT WILL RENDER YOUR NETWORK AND SYSTEMS SAFE FROM MALICIOUS 
+                                CODE, VULNERABILITIES, INTRUSIONS, OR OTHER SECURITY BREACHES, (C) EVERY VULNERABILITY ON EVERY TESTED SYSTEM OR APPLICATION WILL BE DISCOVERED, OR (D) THERE WILL BE NO FALSE POSITIVES.
+                                IN NO EVENT WILL KAMITEE OR ITS LICENSORS BE LIABLE TO YOU OR YOUR EMPLOYEES, OR ANY THIRD PARTY, FOR ANY LOST REVENUE, PROFIT, OR DATA, BUSINESS INTERRUPTION, 
+                                OR FOR SPECIAL, INDIRECT, CONSEQUENTIAL, INCIDENTAL, OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY ARISING OUT OF THE USE OF OR
+                                INABILITY TO USE THE SOFTWARE EVEN IF KAMITEE HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.IN NO EVENT SHALL KAMITEE BE LIABLE TO YOU, WHETHER IN CONTRACT,
+                                WARRANTY OR TORT (INCLUDING NEGLIGENCE OR STRICT LIABILITY),OR OTHERWISE EXCEED THE FEE PAID BY YOU.\n
+
+                            . Term and Termination \n
+                                This License Agreement is effective until terminated or the end of the Term. You may terminate this License Agreement at any time (i) by destroying all 
+                                copies of Software, related documentation, analysis data and report and purging same from memory devices (required at the end of a Term). Your rights 
+                                under this License Agreement will terminate immediately without notice from Kamitee if you fail to comply with any provision of this Agreement. Upon 
+                                any termination, you must destroy all copies of Software and related documentation and purge same from memory devices. All provisions of this License 
+                                Agreement relating to disclaimers of warranties, limitation of liabilities, remedies, damages protection of information and shall survive termination. \n
+
+                              """
             }
             return Response({'data':data,'Response' :True,'Message':''},status=200)
         else:
