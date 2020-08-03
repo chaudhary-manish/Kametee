db95ece2aac39a0a1f3942aa8971070fbda58b0b
Manish Chaudhary <chaudhary94rc@gmail.com>
2020-08-03 18:20:51 +0530

    change get method

--- Kameteeapp/API/views.py
+++ Kameteeapp/API/views.py
@@ -766,7 +766,8 @@ def Group_Payments(request):
             if GroupPaymentHistorydetails.Status == 5:
                 totalAmountDue = int(float(GroupPaymentHistorydetails.ActualAmount)) - int(float(PaidAmount))
                 GroupPaymentHistory.objects.filter(id=id,GroupBidding_id=groupbiddingdetails['id'],Mobilenumber = UserMobileNumber).update(AmountPaid=PaidAmount,AmountDue=totalAmountDue,
-                                                IsReceived = recivedflag )            
+                                                IsReceived = recivedflag ) 
+                return Response({'data':'','Response' :False,'Message' :'Group Payments list not found'},status=200)           
             
             if recivedflag == 1:
                 GroupPaymentHistorydetails = GroupPaymentHistory.objects.filter(GroupBidding_id = groupbiddingdetails['id'],Status =5)
